e9071f18b25cbfd7fe0c81fe9b3d3e099e15490a
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-12-03 23:51:22 +0000

    chore(test): move fixtures to conftest

--- tests/services/test_transaction_service.py
+++ tests/services/test_transaction_service.py
@@ -4,21 +4,6 @@ from tx_lens.domain import Transaction, UnexistingTransaction
 from tx_lens.services import TransactionService
 
 
-@pytest.fixture
-def transaction_hash() -> str:
-    return "0x206d9ed0458f07377c05045b1c9d3e4cdb03c03141f4070f5f88dd512d74dda0"
-
-
-@pytest.fixture
-def unexisting_transaction_hash() -> str:
-    return "0x0000000000000000000000000000000000000000000000000000000000000000"
-
-
-@pytest.fixture
-def transaction_service() -> TransactionService:
-    return TransactionService()
-
-
 @pytest.mark.vcr
 def test_fetch_transaction(
     transaction_service: TransactionService,
